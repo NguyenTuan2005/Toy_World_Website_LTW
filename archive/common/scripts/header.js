22975656b0a275e44022e230a9e8391128466270
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-10-25 21:54:26 +0700

    add nar bar for header

--- archive/common/scripts/header.js
+++ archive/common/scripts/header.js
@@ -3,6 +3,7 @@
     const response = await fetch(file);
     const html = await response.text();
     document.getElementById(id).innerHTML = html;
+    console.log(html);
   }
 
   loadFragment("header", "../../archive/common/header.html");

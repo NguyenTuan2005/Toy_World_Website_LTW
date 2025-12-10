afb50aaafafd79343f64f7c8fb0925cd046220df
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2025-12-10 11:13:55 +0700

    Created some fundamental exceptions

---
+++ src/main/java/com/n3/childrentoyweb/exception/ObjectNotFoundException.java
@@ -0,0 +1,4 @@
+package com.n3.childrentoyweb.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+}
---
+++ src/main/java/com/n3/childrentoyweb/exception/PermissionDeniedException.java
@@ -0,0 +1,4 @@
+package com.n3.childrentoyweb.exception;
+
+public class PermissionDeniedException extends RuntimeException {
+}

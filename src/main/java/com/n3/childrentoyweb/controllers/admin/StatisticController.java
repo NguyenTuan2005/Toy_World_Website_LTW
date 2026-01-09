4866fe7e35d1ac639b46e5bf7f9b3d17501c6981
Nguyễn Hữu Duy <duynguyenavg@gmail.com>
2026-01-09 13:46:22 +0700

    fix bug

--- src/main/java/com/n3/childrentoyweb/controllers/admin/StatisticController.java
+++ src/main/java/com/n3/childrentoyweb/controllers/admin/StatisticController.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@WebServlet(name = "StatisticManage",value = "/statistics")
+@WebServlet(name = "StatisticManage",value = "/admin/statistics")
 public class StatisticController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
